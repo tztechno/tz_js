3301643a979d5581fe39c8db101dc37b69aba7c0
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-04 23:04:36 +0900

    Create download.js

---
+++ download.js
@@ -0,0 +1,14 @@
+//https://github.com/code4fukui/tidelevel/blob/main/download.js
+
+import { DateTime, Day, TimeZone } from "https://js.sabae.cc/DateTime.js";
+import { saveDay } from "./saveDay.js";
+
+const today = new Day(TimeZone.JST);
+const yesterday = today.dayBefore(1);
+const now = new DateTime();
+const daystart = new DateTime(today, TimeZone.JST);
+const dt = daystart.getTime() - now.getTime();
+if (dt > -2 * 60 * 60 * 1000) {
+  await saveDay(yesterday);
+}
+await saveDay(today);
