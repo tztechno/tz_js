346f7a6d41c8b675679a1a71df8d53f5ea30d3b8
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-08 13:04:11 +0900

    Update dateobject.js

--- dateobject.js
+++ dateobject.js
@@ -6,14 +6,14 @@ const input = fs.readFileSync(filePath).toString().split("\n");
 const N = parseInt(input[0]); // 日付の個数
 const T = input.slice(1,1+N); // 日付の文字列配列
 
-function convertDates(T) {
+function convertAndPrintDates(T) {
   const dateObjects = T.map(t_str => new Date(t_str));
   dateObjects.forEach(dateObj => {
-  console.log(dateObj.toISOString());
+    console.log(dateObj.toISOString());
   });
 }
 
-convertDates();
+convertAndPrintDates(T); 
 
 #####################################
 
