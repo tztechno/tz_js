ed2245bd6d0221ece0185cabdb79af2f0ae16eca
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-03 14:43:41 +0900

    Create colorBar.js

---
+++ colorBar.js
@@ -0,0 +1,14 @@
+    <style>
+        #colormapBar {
+            width: 300px;
+            height: 20px;
+            background: linear-gradient(to right, #ffff00 0%, #ff0000 100%);
+            margin: 10px;
+        }
+    </style>
+
+
+
+<body>
+    <div id="colormapBar"></div>
+</body>
