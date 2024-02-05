b471a3d752eae68e11127790552acb3441db1584
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-05 16:07:41 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,132 @@
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+<script src="/js/jquery.js"></script>
+<script id=pt_loader src="/js/ewbc.min.js"></script>
+//////////////////////////////////////////////////////
+<script>
+//<![CDATA[
+!function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function(){q("f",arguments)},setDisplayBids:function(){},targetingKeys:function(){return[]},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore(A,g)}("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
+
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+var headerBiddingSlots = [];
+var nonHeaderBiddingSlots = [];
+var gptAdSlots = [];
+
+(function() {
+googletag.cmd.push(function() {
+
+gptAdSlots[0] = googletag.defineSlot("/13321808/ad_pc1_main_bottom1", ["fluid", [728, 90]], "div-gpt-ad-1521789992487-0").addService(googletag.pubads());
+gptAdSlots[1] = googletag.defineSlot("/13321808/side_topa", [[200, 69], [300, 100]], "div-gpt-ad-1411372846272-4").addService(googletag.pubads());
+gptAdSlots[2] = googletag.defineSlot("/13321808/side_ekenkoshop", [[200, 69], [300, 100]], "div-gpt-ad-1411372846272-0").addService(googletag.pubads());
+gptAdSlots[3] = googletag.defineSlot("/13321808/side_kanshinhiroba", [[200, 50], [200, 69], [300, 100]], "div-gpt-ad-1411372846272-2").addService(googletag.pubads());
+gptAdSlots[4] = googletag.defineSlot("/13321808/side_oisde", [[200, 69], [300, 100]], "div-gpt-ad-1411372846272-1").addService(googletag.pubads());
+gptAdSlots[5] = googletag.defineSlot("/13321808/side_joseinomikata", [200, 83], "div-gpt-ad-1564729411976-0").addService(googletag.pubads());
+gptAdSlots[6] = googletag.defineSlot("/13321808/side_reserve", [[200, 69], [300, 100]], "div-gpt-ad-1411372846272-3").addService(googletag.pubads());
+gptAdSlots[7] = googletag.defineSlot("/13321808/ad_pc8_side1", ["fluid", [300, 250], [336, 280]], "div-gpt-ad-1521797728610-0").addService(googletag.pubads());
+gptAdSlots[8] = googletag.defineSlot("/13321808/ad_pc10_side2", [[300, 250], [336, 280]], "div-gpt-ad-1464329102042-0").addService(googletag.pubads());
+gptAdSlots[9] = googletag.defineSlot("/13321808/ad_pc9_side_bottom1", [[336, 280], [300, 250], [300, 600], "fluid"], "div-gpt-ad-1521797755826-0").addService(googletag.pubads());
+headerBiddingSlots.push(gptAdSlots[0]);
+nonHeaderBiddingSlots.push(gptAdSlots[1]);
+nonHeaderBiddingSlots.push(gptAdSlots[2]);
+nonHeaderBiddingSlots.push(gptAdSlots[3]);
+nonHeaderBiddingSlots.push(gptAdSlots[4]);
+nonHeaderBiddingSlots.push(gptAdSlots[5]);
+nonHeaderBiddingSlots.push(gptAdSlots[6]);
+headerBiddingSlots.push(gptAdSlots[7]);
+headerBiddingSlots.push(gptAdSlots[8]);
+headerBiddingSlots.push(gptAdSlots[9]);
+googletag.pubads().disableInitialLoad();
+googletag.pubads().enableSingleRequest();
+googletag.pubads().collapseEmptyDivs();
+googletag.enableServices();
+if (nonHeaderBiddingSlots.length > 0) {
+googletag.pubads().refresh(nonHeaderBiddingSlots);
+}
+
+apstag.init({
+pubID: "9c0971e2-dbb5-41a9-ab14-58c80b5e0132",
+adServer: "googletag",
+});
+
+apstag.fetchBids(
+{
+slots: [{
+slotID: "div-gpt-ad-1521789992487-0",
+slotName: "/13321808/ad_pc1_main_bottom1",
+sizes: ["fluid", [728, 90]]
+}
+,{
+slotID: "div-gpt-ad-1521797728610-0",
+slotName: "/13321808/ad_pc8_side1",
+sizes: ["fluid", [300, 250], [336, 280]]
+}
+,{
+slotID: "div-gpt-ad-1464329102042-0",
+slotName: "/13321808/ad_pc10_side2",
+sizes: [[300, 250], [336, 280]]
+}
+,{
+slotID: "div-gpt-ad-1521797755826-0",
+slotName: "/13321808/ad_pc9_side_bottom1",
+sizes: [[336, 280], [300, 250], [300, 600], "fluid"]
+}
+],
+timeout: 3000,
+},
+function (bids) {
+apstag.setDisplayBids();
+googletag.pubads().refresh(headerBiddingSlots);
+}
+);
+});
+})();
+
+//]]>
+</script>
+//////////////////////////////////////////////////////
+<script src="/packs/js/application-ea26c67e04d1a857e618.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script><script>
+//<![CDATA[
+window.jQuery || document.write('<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.2.4.min.js"><\/script>');
+//]]>
+</script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script><script>
+//<![CDATA[
+window.jQuery.ui || document.write('<script src="//ajax.aspnetcdn.com/ajax/jquery.ui/1.12.1/jquery-ui.min.js"><\/script>');
+//]]>
+</script>
+<script src="/assets/pc-c3173f55e944ef67818155c10da1fd175b12f6689b5576cb352e705a0fc679c8.js"></script>
+<script src="/assets/page/clinic-5de0a7ca3755de8b8373ae193d828761821a7adef7dfb7819131bf69946f8eb7.js" async="async"></script>
+<script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.5/lazysizes.min.js" async="async"></script>
+<script src="https://byoinnavi.drsquare.jp/js/qa.js?ta=%2Fta%2Fclinic%2Fhyogo%2Fnormal" async="async"></script>
+<script src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" async="async"></script>
+//////////////////////////////////////////////////////
+<script type="application/json">
+{
+"direction": "alternate",
+"duration": "300ms",
+"fill": "both",
+"animations": [{
+"selector": ".sticky_item",
+"easing": "cubic-bezier(.4,0,.2,1)",
+"keyframes": [{
+"opacity": "1",
+"visibility": "visible"
+}]
+}]
+}
+</script>
+//////////////////////////////////////////////////////
+<script>
+//<![CDATA[
+setPowerTipCate("#corp-info__cate-link--9", "corp-info__cate-popup--9");
+//]]>
+</script>
+//////////////////////////////////////////////////////
