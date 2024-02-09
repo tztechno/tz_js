0228588c53e8a1da57ce2b3837bd548e3ff9ff7f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-09 23:16:03 +0900

    Create link_script.js

---
+++ link_script.js
@@ -0,0 +1,27 @@
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+		<link rel="preload" as="script" href="/common/2020/js/bundle.min.js" />
+		<link rel="preload" as="script" href="/common/js/loader.js" />
+		<link rel="preload" as="script" href="/common/js/jquery-1.10.2.min.js" />
+		<link rel="preload" as="script" href="/common/js/jqueryui-1.10.3.min.js" />
+		<link rel="preload" as="script" href="/common/js/jquery.easing.1.3.js" />
+		<link rel="preload" as="script" href="/common/js/jquery.tile.js" />
+		<link rel="preload" as="script" href="/common/js/jquery.carouFredSel-6.0.4-packed.js" />
+		<link rel="preload" as="script" href="/common/js/jquery.touchSwipe.min.js" />
+		<link rel="preload" as="script" href="/common/js/jquery.colorbox-min.js" />
+		<link rel="preload" as="script" href="/common/js/css_browser_selector.js" />
+		<link rel="preload" as="script" href="/common/js/common.js" />
+		<link rel="preload" as="script" href="/common/js/extlink.js" />
+#####################################################
